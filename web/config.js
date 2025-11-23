1bcebbbde1c082390549e0fe778e226b738150d1
Hyunseok Oh <developer@triplengames.com>
2025-11-23 22:07:32 +0900

    Update README.md 7

---
+++ web/config.js
@@ -0,0 +1,5 @@
+// 복사하여 web/config.js 로 이름을 바꾸고 실제 값을 입력하세요.
+window.SUPABASE_CONFIG = {
+    url: "https://awaalyspbkhzoacljanv.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3YWFseXNwYmtoem9hY2xqYW52Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4NTk5NzAsImV4cCI6MjA3OTQzNTk3MH0.rH9Q6KZT2ff-s4g6GAfE4dYAGekOCtQEl_VNY2mjCZ0",
+}
